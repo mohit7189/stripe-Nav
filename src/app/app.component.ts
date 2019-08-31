5c56ecd6854ba049f813474ae82eed7bef3ad5eb
mojain19 <mojain19@in.ibm.com>
2019-08-31 19:49:17 +0530

    New Angular Assignement
    New Angular Assignement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Http } from "@angular/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'stripe';
+  title = "stripe";
+  navMenus: any[];
+  mainMenu: any[];
+
+  constructor(http: Http) {
+    http
+      .get(
+        "https://jsonblob.com/api/jsonBlob/6766327f-607d-11e9-95ef-9bcb815ba4a4"
+      )
+      .subscribe(response => {
+        this.navMenus = response.json();
+        console.log(this.navMenus);
+        this.mainMenu = Object.keys(this.navMenus);
+        console.log(this.mainMenu);
+      });
+  }
 }
